406cda19e3a5195e2795ee7848a04cbf4558a61c
Alexandrov Vladimir Morozov <81023608+YamiyoshinLevi@users.noreply.github.com>
2023-01-10 15:18:47 +0800

    Update schBlocks.java

--- src/SCH/content/schBlocks.java
+++ src/SCH/content/schBlocks.java
@@ -26,21 +26,26 @@ public class schBlocks {
             requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
             health = 16;
             size = 3;
-            placeSound = Vars.tree.loadSound("Anthems/Polska");
+            placeSound = Vars.tree.loadSound("Anthems/SDFE");
+            placePitchChange = false;
         }};
         flagDEU = new Wall("Deutschland") {{
             requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
             health = 16;
             size = 3;
+            placeSound = Vars.tree.loadSound("Anthems/Deutschlandlied");
+            placePitchChange = false;
         }};
         flagPOL = new Wall("Polska") {{
             requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
             health = 16;
             size = 3;
+            placeSound = Vars.tree.loadSound("Anthems/Polska");
+            placePitchChange = false;
         }};
 
         /* SCH Turrets */
-        KavtashkaMKI = new ItemTurret("KavtashkaMKI") {{
+        KavtashkaMKI = new ItemTurret("KavtashkaMKI"){{
             requirements(Category.turret, ItemStack.with(Items.copper, 35, Items.lead, 5));
             ammo(
                 Items.copper,  new BasicBulletType(8f, 10){{
@@ -68,10 +73,10 @@ public class schBlocks {
                 }}
             );
 
-            shoot = new ShootAlternate(2f);
+            shoot = new ShootAlternate(0f);
 
             size = 2;
-            shootY = 3f;
+            shootY = 6f;
             reload = 10f;
             range = 172;
             shootCone = 5f;
@@ -80,6 +85,52 @@ public class schBlocks {
             inaccuracy = 2f;
             rotateSpeed = 10f;
             coolant = consumeCoolant(0.1f);
+            shootSound = Vars.tree.loadSound("Turrets/KavtashkaMKI");
+            researchCostMultiplier = 0.05f;
+
+            limitRange();
+        }};
+        KavtashkaMKII = new ItemTurret("KavtashkaMKII"){{
+            requirements(Category.turret, ItemStack.with(Items.copper, 45, Items.lead, 5));
+            ammo(
+                Items.copper,  new BasicBulletType(8f, 10){{
+                    width = 4f;
+                    height = 9f;
+                    shootEffect = Fx.shootSmall;
+                    smokeEffect = Fx.shootSmallSmoke;
+                    lifetime = 60f;
+                    ammoMultiplier = 4;
+                }},
+                Items.lead, new BasicBulletType(8f, 12){{
+                    width = 9f;
+                    height = 12f;
+                    reloadMultiplier = 0.6f;
+                    ammoMultiplier = 4;
+                    lifetime = 60f;
+                }},
+                Items.graphite, new BasicBulletType(8f, 18){{
+                    width = 7f;
+                    height = 9f;
+                    homingPower = 0.1f;
+                    reloadMultiplier = 1.5f;
+                    ammoMultiplier = 5;
+                    lifetime = 60f;
+                }}
+            );
+
+            shoot = new ShootAlternate(0f);
+
+            size = 2;
+            shootY = 6f;
+            reload = 6f;
+            range = 180;
+            shootCone = 5f;
+            ammoUseEffect = Fx.casing1;
+            health = 336;
+            inaccuracy = 3f;
+            rotateSpeed = 10f;
+            coolant = consumeCoolant(0.1f);
+            shootSound = Vars.tree.loadSound("Turrets/KavtashkaMKII");
             researchCostMultiplier = 0.05f;
 
             limitRange();
