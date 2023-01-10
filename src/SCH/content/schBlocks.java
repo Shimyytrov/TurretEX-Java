0a54eb04b0d195ef16f1ab997b9599a9fdb345e6
Alexandrov Vladimir Morozov <81023608+YamiyoshinLevi@users.noreply.github.com>
2023-01-10 15:56:48 +0800

    Update schBlocks.java

--- src/SCH/content/schBlocks.java
+++ src/SCH/content/schBlocks.java
@@ -1,24 +1,57 @@
 package SCH.content;
 
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import mindustry.*;
 import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
-import mindustry.entities.pattern.ShootAlternate;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.DrawPart.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.type.unit.*;
 import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
 import mindustry.world.blocks.defense.*;
 import mindustry.world.blocks.defense.turrets.*;
-import mindustry.Vars;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.heat.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+import static mindustry.type.ItemStack.*;
 
 public class schBlocks {
 
+    public static Block 
     /* Flags */
-    public static Block flagSCH, flagDEU, flagPOL;
+    flagSCH, flagDEU, flagPOL,
 
     /* SCH Turrets */
-    public static Block KavtashkaMKI, KavtashkaMKII, KavtashkaMKIII, KavtashkaMKIV;
-    public static Block Satan;
-    public static Block GoryMKI, GoryMKII;
-    public static Block Kalinka, Katyusha, KatyushaCHEAT;
+    KavtashkaMKI, KavtashkaMKII, KavtashkaMKIII, KavtashkaMKIV,
+    Satan,
+    GoryMKI, GoryMKII,
+    Kalinka, Katyusha, KatyushaCHEAT
+    ;
 
     public static void load() {
         /* Flags */
