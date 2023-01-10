905cfe0464934ded7ee1c1b66c11670d0bd6cb61
Alexandrov Vladimir Morozov <81023608+YamiyoshinLevi@users.noreply.github.com>
2023-01-10 14:49:48 +0800

    Update schBlocks.java

--- src/SCH/content/schBlocks.java
+++ src/SCH/content/schBlocks.java
@@ -1,22 +1,32 @@
 package SCH.content;
 
-import mindustry.content.Items;
-import mindustry.type.Category;
-import mindustry.type.ItemStack;
-import mindustry.world.Block;
-import mindustry.world.blocks.defense.Wall;
-import arc.assets.Sounds;
+import mindustry.content.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.pattern.ShootAlternate;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.Vars;
+
 public class schBlocks {
 
-    /** Flags */
+    /* Flags */
     public static Block flagSCH, flagDEU, flagPOL;
 
+    /* SCH Turrets */
+    public static Block KavtashkaMKI, KavtashkaMKII, KavtashkaMKIII, KavtashkaMKIV;
+    public static Block Satan;
+    public static Block GoryMKI, GoryMKII;
+    public static Block Kalinka, Katyusha, KatyushaCHEAT;
+
     public static void load() {
+        /* Flags */
         flagSCH = new Wall("Schmitrovland") {{
             requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
             health = 16;
             size = 3;
-            placeSound = Sounds.
+            placeSound = Vars.tree.loadSound("Anthems/Polska");
         }};
         flagDEU = new Wall("Deutschland") {{
             requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
@@ -27,6 +37,52 @@ public class schBlocks {
             requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
             health = 16;
             size = 3;
+        }};
+
+        /* SCH Turrets */
+        KavtashkaMKI = new ItemTurret("KavtashkaMKI") {{
+            requirements(Category.turret, ItemStack.with(Items.copper, 35, Items.lead, 5));
+            ammo(
+                Items.copper,  new BasicBulletType(8f, 10){{
+                    width = 4f;
+                    height = 9f;
+                    shootEffect = Fx.shootSmall;
+                    smokeEffect = Fx.shootSmallSmoke;
+                    lifetime = 60f;
+                    ammoMultiplier = 2;
+                }},
+                Items.lead, new BasicBulletType(8f, 12){{
+                    width = 9f;
+                    height = 12f;
+                    reloadMultiplier = 0.6f;
+                    ammoMultiplier = 4;
+                    lifetime = 60f;
+                }},
+                Items.graphite, new BasicBulletType(8f, 18){{
+                    width = 7f;
+                    height = 9f;
+                    homingPower = 0.1f;
+                    reloadMultiplier = 1.5f;
+                    ammoMultiplier = 5;
+                    lifetime = 60f;
+                }}
+            );
+
+            shoot = new ShootAlternate(2f);
+
+            size = 2;
+            shootY = 3f;
+            reload = 10f;
+            range = 172;
+            shootCone = 5f;
+            ammoUseEffect = Fx.casing1;
+            health = 300;
+            inaccuracy = 2f;
+            rotateSpeed = 10f;
+            coolant = consumeCoolant(0.1f);
+            researchCostMultiplier = 0.05f;
+
+            limitRange();
         }};
     };
 }; 
