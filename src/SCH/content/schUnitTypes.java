e49e7c3ebe4afed8034a1749df4e1858e70e7fb3
Alexandrov Vladimir Morozov <81023608+YamiyoshinLevi@users.noreply.github.com>
2023-03-24 22:04:14 +0800

    2

--- src/SCH/content/schUnitTypes.java
+++ src/SCH/content/schUnitTypes.java
@@ -72,5 +72,67 @@ public class schUnitTypes {
             }});
             constructor = UnitEntity::create;
         }};
+        RukhtuskaMKII = new UnitType("RukhtuskaMKI"){{
+            speed = 5f;
+            accel = 0.1f;
+            drag = 0.02f;
+            flying = true;
+            health = 440f;
+            engineOffset = 6f;
+            hitSize = 9f;
+            itemCapacity = 70;
+            targetAir = true;
+            targetGround = true;
+            faceTarget = true;
+            autoFindTarget = true;
+            forceMultiTarget = true;
+            circleTarget = true;
+            rotateToBuilding = true;
+            buildSpeed = 2f;
+            buildRange = 256f;
+            defaultCommand = UnitCommand.rebuildCommand;
+            weapons.add(new Weapon("RukhtustiskayaGewehrMKII"){{
+                y = 0;
+                x = 4;
+                mirror = true;
+                rotate = false;
+                reload = 8;
+                recoil = 8;
+                ejectEffect = casing2;
+                shake = 2;
+                bullet = new BasicBulletType(16f, 36){{
+                    width = 5f;
+                    height = 48f;
+                    lifetime = 12f;
+                    shootEffect = Fx.shootSmall;
+                    smokeEffect = shootSmallSmoke;
+                    ammoMultiplier = 2;
+                    frontColor = Color.valueOf("ffffff");
+                    backColor = Color.valueOf("00ff00f");
+                    lightColor = Color.valueOf("00ff00");
+                }};
+                shootSound = Vars.tree.loadSound("Turrets/KavtashkaMKII");
+            }});
+            weapons.add(new Weapon("RukhtustiskayaArtylleryMKI"){{
+                y = 0;
+                x = 0;
+                mirror = false;
+                rotate = true;
+                reload = 20;
+                recoil = 2;
+                ejectEffect = casing2;
+                shake = 2;
+                bullet = new ArtilleryBulletType(4.8f, 36){{
+                    width = 12f;
+                    height = 16f;
+                    lifetime = 60f;
+                    shootEffect = Fx.shootBig;
+                    smokeEffect = shootSmallSmoke;
+                    ammoMultiplier = 2;
+                }};
+                shootSound = Vars.tree.loadSound("Turrets/AA-sg");
+            }});
+            constructor = UnitEntity::create;
+        }};
     }
 };
