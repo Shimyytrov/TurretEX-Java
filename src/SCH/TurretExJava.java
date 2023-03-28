079ec362cc06fb6bdfb17b96c5ac89e1a326cc83
Alexandrov Vladimir Morozov <81023608+YamiyoshinLevi@users.noreply.github.com>
2023-03-28 13:15:00 +0800

    3

--- src/SCH/TurretExJava.java
+++ src/SCH/TurretExJava.java
@@ -1,12 +1,26 @@
 package SCH;
 
 import SCH.content.schUnitTypes;
+
+import java.util.Locale;
+
 import SCH.content.schBlocks;
 import SCH.content.schTechTree;
+import SCH.ui.dialogs.LanguageDialog;
+import arc.*;
+import arc.struct.*;
+import mindustry.Vars;
+import mindustry.game.EventType.*;
 import mindustry.mod.Mod;
 
 public class TurretExJava extends Mod{
     public TurretExJava(){
+        if(!Vars.headless){
+            Events.on(ContentInitEvent.class, e -> {
+              Vars.locales = Seq.with(Vars.locales).add(new Locale("SCH")).toArray(Locale.class);
+              LanguageDialog.displayNames.put("SCH", "Schmitŕovkiy");
+            });
+          }
     }
 
     @Override
